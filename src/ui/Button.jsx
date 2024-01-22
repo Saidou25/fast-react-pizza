11b3ddc891b25ded53d39a0633fb7b92d7547519
Saidou25 <115666389+Saidou25@users.noreply.github.com>
2024-01-22 07:27:54 -0500

    styled order form and added pizza emoji on tab

--- src/ui/Button.jsx
+++ src/ui/Button.jsx
@@ -3,11 +3,11 @@ import { Link } from 'react-router-dom'
 
 export default function Button({ children, disabled, to, type }) {
 
-const base = 'inline-block bg-yellow-400 uppercase font-semibold text-stone-800 rounded-full tracking-widest hover:bg-yellow-300 transition-colors-300 duration-300 focus:outline-none focus:ring focus:ring-yellow-300 focus:bg-yellow-300 focus:ring-offset-2 disabled:cursor-not-allowed '
+const base = 'inline-block text-sm  bg-yellow-400 uppercase font-semibold text-stone-800 rounded-full tracking-widest hover:bg-yellow-300 transition-colors-300 duration-300 focus:outline-none focus:ring focus:ring-yellow-300 focus:bg-yellow-300 focus:ring-offset-2 disabled:cursor-not-allowed '
 const styles = {
   primary: base + "px-4 py-3 md:px-6 md:py-4",
   small: base + "px-4  py-2 md:px-5 md:py-2.5 text-xs",
-  secondary: 'inline-block border-2 border-stone-300 uppercase font-semibold text-stone-400 rounded-full tracking-widest hover:bg-stone-300 hover:text-stone-800 transition-colors-300 duration-300 focus:outline-none focus:text-stone-800 focus:ring focus:ring-stone-200 focus:bg-stone-300 focus:ring-offset-2 disabled:cursor-not-allowed px-4 py-2.5 md:px-6 md:py-3.5'
+  secondary: 'inline-block text-sm border-2 border-stone-300 uppercase font-semibold text-stone-400 rounded-full tracking-widest hover:bg-stone-300 hover:text-stone-800 transition-colors-300 duration-300 focus:outline-none focus:text-stone-800 focus:ring focus:ring-stone-200 focus:bg-stone-300 focus:ring-offset-2 disabled:cursor-not-allowed px-4 py-2.5 md:px-6 md:py-3.5'
  
 };
    

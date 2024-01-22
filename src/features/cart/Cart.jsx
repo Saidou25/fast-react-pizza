e850b2d23dbb0cde4873c3d58cf9adb89b993867
Saidou25 <115666389+Saidou25@users.noreply.github.com>
2024-01-22 09:54:32 -0500

    finished working on user's global state

--- src/features/cart/Cart.jsx
+++ src/features/cart/Cart.jsx
@@ -2,6 +2,7 @@ import { Link } from "react-router-dom";
 import LinkButton from "../../ui/LinkButton";
 import Button from "../../ui/Button";
 import CartItem from "./CartItem";
+import { useSelector } from "react-redux";
 
 const fakeCart = [
   {
@@ -28,16 +29,17 @@ const fakeCart = [
 ];
 
 function Cart() {
+  const username = useSelector((state) => state.user.username);
   const cart = fakeCart;
 
   return (
     <div className="py-3 px-4">
       <LinkButton to="/menu">&larr; Back to menu</LinkButton>
 
-      <h2 className="mt-7 text-xl font-semiblod">Your cart, %NAME%</h2>
+      <h2 className="mt-7 text-xl font-semiblod">Your cart, {username}</h2>
       <ul className="divide-y divide-stone-200 border-b border-stone-200 mt-3">
         {cart.map((item) => (
-          <CartItem item={item} key={item.key} />
+          <CartItem item={item} key={item.id} />
         ))}
       </ul>
       <div className="mt-6 space-x-2">

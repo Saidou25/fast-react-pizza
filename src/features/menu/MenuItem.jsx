5a81166fa09f311ea400ad86da105eee632cf099
Saidou25 <115666389+Saidou25@users.noreply.github.com>
2024-01-22 14:08:44 -0500

    worked on ADD TO CART dispatch

--- src/features/menu/MenuItem.jsx
+++ src/features/menu/MenuItem.jsx
@@ -1,23 +1,53 @@
 import React from "react";
 import { formatCurrency } from "../../utils/helpers";
 import Button from "../../ui/Button";
+import { useDispatch } from "react-redux";
+import { addItem } from "../cart/cartSlice";
 
 function MenuItem({ pizza }) {
-    const { id, name, unitPrice, ingredients, soldOut, imageUrl } = pizza;
-  
-    return (
-      <li className="flex gap-4 py-2">
-        <img src={imageUrl} alt={name} className={`h-24 ${soldOut ? "opacity-70 grayscale" : ""}`} />
-        <div className="flex flex-col grow pt-0.5">
-          <p className="font-mediium">{name}</p>
-          <p className="text-sm italic text-stone-500 capitalize">{ingredients.join(', ')}</p>
-          <div className="mt-auto flex justify-between items-center">
-            {!soldOut ? <p className="text-sm">{formatCurrency(unitPrice)}</p> : <p className="text-sm uppercase font-medium text-stone-500">Sold out</p>}
-            <Button type="small">Add to cart</Button>
-          </div>
-        </div>
-      </li>
-    );
+  const dispatch = useDispatch();
+
+  const { id, name, unitPrice, ingredients, soldOut, imageUrl } = pizza;
+
+  function handleAddToCart() {
+    const newItem = {
+      pizzaId: id,
+      name,
+      quantity: 1,
+      unitPrice,
+      totalPrice: unitPrice * 1,
+    };
+    dispatch(addItem(newItem));
   }
-  
-  export default MenuItem;
+  return (
+    <li className="flex gap-4 py-2">
+      <img
+        src={imageUrl}
+        alt={name}
+        className={`h-24 ${soldOut ? "opacity-70 grayscale" : ""}`}
+      />
+      <div className="flex flex-col grow pt-0.5">
+        <p className="font-mediium">{name}</p>
+        <p className="text-sm italic text-stone-500 capitalize">
+          {ingredients.join(", ")}
+        </p>
+        <div className="mt-auto flex justify-between items-center">
+          {!soldOut ? (
+            <p className="text-sm">{formatCurrency(unitPrice)}</p>
+          ) : (
+            <p className="text-sm uppercase font-medium text-stone-500">
+              Sold out
+            </p>
+          )}
+          {!soldOut && (
+            <Button type="small" onClick={handleAddToCart}>
+              Add to cart
+            </Button>
+          )}
+        </div>
+      </div>
+    </li>
+  );
+}
+
+export default MenuItem;
--- src/ui/Button.jsx
+++ src/ui/Button.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
 
-export default function Button({ children, disabled, to, type }) {
+export default function Button({ children, disabled, to, type, onClick }) {
 
 const base = 'inline-block text-sm  bg-yellow-400 uppercase font-semibold text-stone-800 rounded-full tracking-widest hover:bg-yellow-300 transition-colors-300 duration-300 focus:outline-none focus:ring focus:ring-yellow-300 focus:bg-yellow-300 focus:ring-offset-2 disabled:cursor-not-allowed '
 const styles = {
@@ -14,6 +14,11 @@ const styles = {
   if (to) {
     return <Link to={to} className={styles[type]}>{children}</Link>
   }
+  if (onClick) 
+  return (
+<button onClick={onClick} disabled={disabled} className={styles[type]} type={type}>
+        {children} 
+     </button>)
   return (
     <button disabled={disabled} className={styles[type]} type={type}>
         {children} 

da695a4408c73b2c58007430ae92a0b7df1ffa45
Saidou25 <115666389+Saidou25@users.noreply.github.com>
2024-01-22 04:56:02 -0500

    created reusable Button, ButtonLink and Link reusable components and classNames to go along with em

---
+++ src/ui/Button.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+
+export default function Button({ children, disabled, to }) {
+  const className='inline-block bg-yellow-400 uppercase font-semibold text-stone-800 py-3 px-4 rounded-full tracking-widest hover:bg-yellow-300 transition-colors-300 duration-300 focus:outline-none focus:ring focus:ring-yellow-300 focus:bg-yellow-300 focus:ring-offset-2 disabled:cursor-not-allowed sm:px-6 sm:py-4'
+
+   
+  if (to) {
+    return <Link to={to} className={className}>{children}</Link>
+  }
+  return (
+    <button disabled={disabled} className={className}>
+        {children}
+     </button>
+  )
+}

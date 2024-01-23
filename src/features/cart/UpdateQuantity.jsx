fdf58e7a176cc1da62bc03b1775322a32e6be9ba
Saidou25 <115666389+Saidou25@users.noreply.github.com>
2024-01-23 12:35:03 -0500

    worked on updating cart item quantity, added - and + buttons for that purpose and dispatch decrease and increaseItemQuantity

---
+++ src/features/cart/UpdateQuantity.jsx
@@ -0,0 +1,25 @@
+import React from "react";
+import Button from "../../ui/Button";
+import { useDispatch } from "react-redux";
+import { decreaseItemQuantity, increaseItemQuantity } from "./cartSlice";
+
+export default function UpdateQuantity({ pizzaId }) {
+  const dispatch = useDispatch();
+
+  return (
+    <div className="flex items-center md:gap-3 gap-1">
+      <Button
+        type="round"
+        onClick={() => dispatch(decreaseItemQuantity(pizzaId))}
+      >
+        -
+      </Button>
+      <Button
+        type="round"
+        onClick={() => dispatch(increaseItemQuantity(pizzaId))}
+      >
+        +
+      </Button>
+    </div>
+  );
+}
